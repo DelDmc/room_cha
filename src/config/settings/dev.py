73770367ddfc1ea598518e94aa718304e42d413e
K.S <del12dmc@gmail.com>
2022-08-24 02:04:40 +0300

    Setup project, settings division, setup black and flake8, installed channels, created app chat

---
+++ src/config/settings/dev.py
@@ -0,0 +1,6 @@
+# DEV
+from config.settings.base import *  # noqa:
+
+DEBUG = True
+
+CURRENT_ENV = "DEV"
---
+++ src/config/settings/main.py
@@ -0,0 +1,6 @@
+# MAIN
+from config.settings.base import *  # noqa:
+
+DEBUG = False
+
+CURRENT_ENV = "MAIN"
